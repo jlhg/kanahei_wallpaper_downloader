cf86e34a135b3c83d21b53b98d2683d509b5d099
Jian-Long Huang <huang@jianlong.org>
2019-07-02 19:02:06 +0800

    Add Gemfile

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
+#!/usr/bin/env ruby
+
 require "nokogiri"
 require "open-uri"
-require "pry"
 require "concurrent"
 require "fileutils"
 
@@ -13,7 +14,6 @@ base_output_dir = "output"
   doc.css("a[href^='http://www.kanahei.com/upload']").each do |element|
     url = element.attr("href")
     m = url.match %r{http://www.kanahei.com/upload/(\d+)/(\d+)/(.+)(4x3|16x9|800_600|1280_1024|1920_1200|1600_1200|1024_768)(.jpg)}
-    binding.pry if m.nil?
     file_name = "#{m[1]}_#{m[2]}_#{m[3]}#{m[4]}#{m[5]}"
     output_file_path = File.join(base_output_dir, m[4], file_name)
     FileUtils.mkdir_p File.join(base_output_dir, m[4])
